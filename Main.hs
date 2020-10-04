fac9a73565c48d4b04361f0ea5fb612db54c2c97
Murat Kasimov <iokasimov.m@gmail.com>
2020-10-04 09:51:25 +0300

    Try indentation based parser

--- Main.hs
+++ Main.hs
@@ -1,3 +1,14 @@
 module Main where
 
-main = print "typechecked"
+import "parsec" Text.Parsec (ParsecT, try, many, parse, noneOf, char, count)
+
+data TT = TT String [TT] deriving Show
+
+indented :: Monad m => Int -> ParsecT String u m TT
+indented level = TT <$> try item <*> many (indented $ level + 1) where
+
+	item :: Monad m => ParsecT String u m String
+	item = count level (char '\t') *> many (noneOf "\t\n") <* char '\n'
+
+main :: IO ()
+main = readFile "resources/example.txt" >>= print . parse (indented 0) ""
