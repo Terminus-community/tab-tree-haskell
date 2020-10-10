3288471b932a6dabdd1566f61c28f62a4dd5a0a0
Murat Kasimov <iokasimov.m@gmail.com>
2020-10-10 10:27:35 +0300

    Parse item properties as well

--- Main.hs
+++ Main.hs
@@ -2,11 +2,15 @@ module Main where
 
 import "attoparsec" Data.Attoparsec.Text
 import "base" Control.Applicative (many)
-import "text" Data.Text (Text)
+import "joint" Control.Joint ((<$$>), (<$$$>))
+import "text" Data.Text (Text, pack)
 
 import qualified "text" Data.Text.IO as T
 
 data TT a = TT a [TT a] deriving Show
+
+instance Functor TT where
+	fmap f (TT x xs) = TT (f x) $ f <$$> xs
 
 indented :: Int -> Parser (TT Text)
 indented level = TT <$> subitem <*> many (indented $ level + 1) where
@@ -14,5 +18,21 @@ indented level = TT <$> subitem <*> many (indented $ level + 1) where
 	subitem :: Parser Text
 	subitem = count level (char '\t') *> takeTill (== '\n') <* (char '\n')
 
+type Predicate = Text
+
+type Object = Text
+
+data Property = Property Predicate Object deriving Show
+
+type Name = Text
+
+data Item = Item Name [Property] deriving Show
+
+item :: Parser Item
+item = Item <$> takeTill (== ' ') <*> many (space *> property)
+
+property :: Parser Property
+property = Property <$> takeTill (== ':') <*> (char ':' *> takeTill (== ' '))
+
 main :: IO ()
-main = T.readFile "resources/example.txt" >>= print . parseOnly (indented 0)
+main = print =<< parseOnly item <$$$> parseOnly (indented 0) <$> T.readFile "resources/example.txt"
